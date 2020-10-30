eb4d60d6d9ccaf8597d1db925cecbca25e62f6b8
sand116 <sandsand1166@gmail.com>
2020-10-30 13:15:38 +0900

    OthelloRandomAdd

--- Othello/Othello.cpp
+++ Othello/Othello.cpp
@@ -1,7 +1,8 @@
 #include <bangtal>
+#include <vector>
+#include <time.h>
 using namespace bangtal;
-
-
+using namespace std;
 
 ObjectPtr board[8][8];
 
@@ -23,10 +24,17 @@ enum class Turn
 	BLACK,
 	WHITE
 };
+typedef struct _Pos
+{
+	_Pos() {}
+	_Pos(int _x, int _y) :x(_x), y(_y) {}
+	int x,y;
+}Pos;
 
 // turn이 모든 메서드에서 쓰이니 일단 전역으로 생성
 Turn turn = Turn::BLACK;
-
+// 흰돌이 선택 가능한 좌표를 저장하는 함수
+vector<Pos> whiteSelect;
 void setState(int x, int y, State state)
 {
 	switch (state)
@@ -44,7 +52,6 @@ void setState(int x, int y, State state)
 bool check(int x, int y, int dx, int dy,bool isIn)
 {
 	if ( (x<0) || (y<0) || (x > 8) || (y > 8)) return false;
-
 	if (board_state[y][x] == State::BLANK) return false;
 
 	if (turn == Turn::BLACK)
@@ -75,46 +82,9 @@ bool check(int x, int y, int dx, int dy,bool isIn)
 	}
 	return false;
 }
-bool checkPossible(int x, int y)
-{
 
-	// 각 방향에 대한 배열을 만들고 각 방향에 대해서 탐색한다
-	int delta[8][2] = { {0,1},{1,0},{-1,0},{0,-1},{1,1} ,{-1,-1},{1,-1},{-1,1} };
-	
-	if (board_state[y][x] == State::BLACK) return false;
-	if (board_state[y][x] == State::WHITE) return false;
 
-	//놓여진 상태가 아니면 블랭크로 변경하고 체크하기
-	setState(x, y, State::BLANK);
-
-	for (auto n : delta)
-	{
-		if (check(x+n[0] , y+n[1] , n[0], n[1], false)) return true;
-	}
-	
-
-	return false;
-}
-
-//8x8 놓을 수 있는 곳 표시 
-bool setPossible()
-{
-	bool possible = false;
-	for (int y = 0; y < 8; y++)
-	{
-		for (int x = 0; x < 8; x++)
-		{	//해당 X,Y에 놓을 수 있는지 확인하고 TRUE이면 변경
-			if (checkPossible(x, y))
-			{
-				setState(x, y, State::POSSIBLE);
-				possible = true;
-
-			}
-		}
-	}
-	return possible;
-}
-
+// 재귀로 구현해보았다.
 void reverseCheck(int x, int y, int dx, int dy, int count)
 {
 	if ((x < 0) || (y < 0) || (x > 8) || (y > 8)) return;
@@ -123,7 +93,7 @@ void reverseCheck(int x, int y, int dx, int dy, int count)
 
 	if (turn == Turn::BLACK)
 	{
-		if (board_state[y][x] == State::WHITE) reverseCheck(x + dx, y + dy, dx, dy,count+1);
+		if (board_state[y][x] == State::WHITE) reverseCheck(x + dx, y + dy, dx, dy, count + 1);
 		else if (board_state[y][x] == State::BLACK)
 		{
 			if (count != 0)
@@ -131,7 +101,7 @@ void reverseCheck(int x, int y, int dx, int dy, int count)
 				for (int i = count; i > 0; i--)
 				{
 					x = x - dx; y = y - dy;
-					setState( x, y , State::BLACK);
+					setState(x, y, State::BLACK);
 				}
 
 			}
@@ -141,7 +111,7 @@ void reverseCheck(int x, int y, int dx, int dy, int count)
 
 	else if (turn == Turn::WHITE)
 	{
-		if (board_state[y][x] == State::BLACK) reverseCheck(x + dx, y + dy, dx, dy, count+1);
+		if (board_state[y][x] == State::BLACK) reverseCheck(x + dx, y + dy, dx, dy, count + 1);
 		else if (board_state[y][x] == State::WHITE)
 		{
 			if (count != 0)
@@ -165,18 +135,95 @@ void reverse(int x, int y)
 
 	for (auto n : delta)
 	{
-		reverseCheck(x+n[0], y+n[1], n[0], n[1], 0);
+		reverseCheck(x + n[0], y + n[1], n[0], n[1], 0);
 	}
 
 	return;
 }
 
+bool checkPossible(int x, int y)
+{
+
+	// 각 방향에 대한 배열을 만들고 각 방향에 대해서 탐색한다
+	int delta[8][2] = { {0,1},{1,0},{-1,0},{0,-1},{1,1} ,{-1,-1},{1,-1},{-1,1} };
+	
+	if (board_state[y][x] == State::BLACK) return false;
+	if (board_state[y][x] == State::WHITE) return false;
+
+	//놓여진 상태가 아니면 블랭크로 변경하고 체크하기
+	setState(x, y, State::BLANK);
+
+	for (auto n : delta)
+	{
+		if (check(x+n[0] , y+n[1] , n[0], n[1], false)) return true;
+	}
+	
+
+	return false;
+}
+
+//8x8 놓을 수 있는 곳 표시 
+Pos* randomSelect()
+{
+	int random = rand() % whiteSelect.size();
+	return &(whiteSelect.at(random));
+}
+
+bool setBlackPossible()
+{
+	bool possible = false;
+	for (int y = 0; y < 8; y++)
+	{
+		for (int x = 0; x < 8; x++)
+		{	//해당 X,Y에 놓을 수 있는지 확인하고 TRUE이면 변경
+			if (checkPossible(x, y) )
+			{
+				setState(x, y, State::POSSIBLE);
+				possible = true;
+			}
+		}
+	}
+	return possible;
+}
+
+bool setWhitePossible()
+{
+	bool possible = false;
+
+	for (int y = 0; y < 8; y++)
+	{
+		for (int x = 0; x < 8; x++)
+		{
+			if (checkPossible(x, y))
+			{
+				whiteSelect.push_back(Pos(x,y));
+			}
+		}
+	}
+
+	if (!whiteSelect.empty())
+	{
+		Pos* random = randomSelect();
+		setState(random->x, random->y, State::WHITE);
+		reverse(random->x, random->y);
+		whiteSelect.resize(0);
+		possible = true;
+		turn = Turn::BLACK;
+	}
+
+	return possible;
+}
+
+
 int main()
 {
 	setGameOption(GameOption::GAME_OPTION_ROOM_TITLE, false);
 	setGameOption(GameOption::GAME_OPTION_MESSAGE_BOX_BUTTON, false);
-	setGameOption(GameOption::GAME_OPTION_ROOM_TITLE, false);
+	setGameOption(GameOption::GAME_OPTION_INVENTORY_BUTTON, false);
 
+	srand((unsigned int)time(NULL));
+	srand(time(0));
+	auto whiteTime = Timer::create(2.f);
 	auto scene = Scene::create("Othello", "Images/background.png");
 
 	for (int y = 0; y < 8; y++)
@@ -187,25 +234,41 @@ int main()
 			board_state[y][x] = State::BLANK;
 
 			board[y][x]->setOnMouseCallback([&,x ,y](ObjectPtr object, int, int, MouseAction) ->bool
-			{if(board_state[y][x]==State::POSSIBLE)
-				{
-					if (turn == Turn::BLACK)
-					{
-						setState(x, y, State::BLACK);
-						reverse(x,y);
-						turn = Turn::WHITE;
-					}
-					else
-					{
-						setState(x, y, State::WHITE);
-						reverse(x,y);
-						turn = Turn::BLACK;
-					}
-					if (!setPossible())
-					{
-						turn == Turn::BLACK ? Turn::WHITE : Turn::WHITE;
-						if (!setPossible()) showMessage("게임 종료");
-					}
+			{	// 돌을 선택할 수 있는 경우는 turn이 검은 돌일 때 뿐이다.
+				if(board_state[y][x]==State::POSSIBLE && turn == Turn::BLACK)
+				{		
+					setState(x, y, State::BLACK);
+					reverse(x,y);
+
+					turn = Turn::WHITE;
+
+
+					whiteTime->start();
+					showTimer(whiteTime);
+					// 흰색돌이 setWhitePossible함수에서 둘 수 있는 곳을 찾고 자동으로 둔 후 턴을 변경한다
+					
+					whiteTime->setOnTimerCallback([&](TimerPtr)->bool 
+					{	//만약 흰색이 둘 수 있는 곳이 존재하지 않는 다면 검은 돌을 둘 곳을 탐색하고 둘 다 없다면 false.
+							if (!setWhitePossible())
+							{
+								turn = Turn::BLACK;
+								if (!setBlackPossible()) showMessage("게임 종료");
+							}
+							// 흰색이 돌을 두었다면 턴을 변경하고 검은 돌이 둘 수 있는 곳을 찾는다. 만약 존재하지 않는 다면 다시 흰색 돌을 둘 수 있는 곳을 찾고 둘 다 존재하지 않는 경우 게임종료.
+							else
+							{
+								turn = Turn::BLACK;
+								if (!setBlackPossible())
+								{
+									turn = Turn::WHITE;
+									if (!setWhitePossible()) showMessage("게임 종료");
+								}
+							}
+						whiteTime->set(2.f);
+						return false;
+					});
+				
+					
 				}
 				
 				
@@ -220,7 +283,7 @@ int main()
 	setState(3, 4, State::WHITE);
 	setState(4, 3, State::WHITE);
 
-	setPossible();
+	setBlackPossible();
 	startGame(scene);
 
 }
